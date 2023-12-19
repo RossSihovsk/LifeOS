18cc88c6a940f90daf7c399403f40912bdb77a07
Veto <vetosew@gmail.com>
2023-12-19 21:57:19 +0200

    push it

--- composeApp/src/androidMain/kotlin/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/Platform.android.kt
@@ -3,5 +3,5 @@ import android.os.Build
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
-
+//lox
 actual fun getPlatform(): Platform = AndroidPlatform()
