676b0f967b26c2095656ca1fdc62b279e08a5245
RossSihovsk <rsihovskiy@gmail.com>
2024-11-01 23:55:57 +0200

    BottomBar.kt minor improvement

--- composeApp/src/commonMain/kotlin/com/project/lifeos/ui/bottomNavigation/BottomBar.kt
+++ composeApp/src/commonMain/kotlin/com/project/lifeos/ui/bottomNavigation/BottomBar.kt
@@ -1,16 +1,5 @@
 package com.project.lifeos.ui.bottomNavigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -19,7 +8,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.navigator.Navigator
 import co.touchlab.kermit.Logger
 import com.project.lifeos.di.AppModule
@@ -34,8 +22,6 @@ expect fun bottomBarNavigation(
     onDoneOrDismiss: () -> Unit
 )
 
-
-
 private val items = BottomBarItems.entries.toList()
 
 @Composable
@@ -49,11 +35,10 @@ fun BottomBar(navigator: Navigator, appModule: AppModule) {
         logger.d("DoneOrDismiss called previousItem: $previousItem selectedItem:$selectedItem")
         selectedItem = previousItem
         previousItem = BottomBarItems.HOME
-        logger.d("DoneOrDismiss After previousItem: $previousItem selectedItem:$selectedItem")
     }
 
     NavigationBar {
-        items.forEach() { item ->
+        items.forEach { item ->
             NavigationBarItem(selected = item == selectedItem,
                 onClick = {
                     logger.d("onClick item: $item selectedItem:$selectedItem")
