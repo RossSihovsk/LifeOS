075613d4c31864f9b0f9a3572f331e59a53d0c9c
VetoForest <94835713+VetoForest@users.noreply.github.com>
2024-11-02 14:15:30 +0200

    [TASK] [DESKTOP] Update according to new changes (#36)

--- composeApp/src/desktopMain/kotlin/com/project/lifeos/ui/bottomNavigation/BottomBar.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/project/lifeos/ui/bottomNavigation/BottomBar.desktop.kt
@@ -3,9 +3,30 @@ package com.project.lifeos.ui.bottomNavigation
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.project.lifeos.di.AppModule
+import com.project.lifeos.ui.screen.AddTaskScreen
+import com.project.lifeos.ui.screen.GoalScreen
+import com.project.lifeos.ui.screen.HomeScreen
+import com.project.lifeos.utils.safePush
 
 
 @Composable
-actual fun bottomBarNavigation(bottomBarItems: BottomBarItems, navigator: Navigator, appModule: AppModule, onDoneorDissmiss: () -> Unit) {
+actual fun bottomBarNavigation(    bottomBarItems: BottomBarItems,
+                                   navigator: Navigator,
+                                   appModule: AppModule,
+                                   onDoneOrDismiss: () -> Unit) {
 
-}
+        val viewModel = appModule.addTaskViewModel
+
+        when (bottomBarItems) {
+            BottomBarItems.HOME -> {
+                navigator.safePush(HomeScreen(appModule))
+            }
+
+            BottomBarItems.GOALS -> {
+                navigator.safePush(GoalScreen(appModule))
+            }
+
+            BottomBarItems.ADD_TASK -> {navigator.safePush(AddTaskScreen(appModule))}
+            BottomBarItems.STATS -> {}
+            BottomBarItems.PROFILE -> {}
+}}
