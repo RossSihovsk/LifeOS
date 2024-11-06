493a14cb1066c04e4f9e5ab6e191293e0c2f53a8
Veto <vetosew@gmail.com>
2024-11-06 11:59:36 +0200

    Window size

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,12 +1,22 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Description
+import androidx.compose.material3.Icon
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.project.lifeos.App
 import com.project.lifeos.di.DesktopAppModule
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "LifeOS") {
+        val state = rememberWindowState(position = WindowPosition(Alignment.Center), size = DpSize(568.dp, 900.dp))
+        Window(onCloseRequest = ::exitApplication, title = "LifeOS", state = state )
+         {
         val appModule = DesktopAppModule()
         App(appModule)
     }
