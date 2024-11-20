b36b4bee22cd20f600189492efc70ff854dc6768
RossSihovsk <rsihovskiy@gmail.com>
2024-11-20 23:44:12 +0200

    [TASK] GoalScreen base design

--- composeApp/src/androidMain/kotlin/com/project/lifeos/ui/screen/GoalScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/project/lifeos/ui/screen/GoalScreen.android.kt
@@ -1,20 +1,244 @@
 package com.project.lifeos.ui.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.project.lifeos.R
+import com.project.lifeos.data.Category
+import com.project.lifeos.data.Goal
+import java.time.LocalDate
+
+val list = listOf(
+    Goal(
+        title = "Improve English",
+        shortPhrase = "Improve my english for work test",
+        category = Category.STUDY,
+        startDate = LocalDate.now(),
+        endDate = LocalDate.now(),
+        tasksId = emptyList()
+    ),
+    Goal(
+        title = "GYM",
+        shortPhrase = "I wanna make gym my habit",
+        category = Category.SPORT,
+        startDate = LocalDate.now(),
+        endDate = LocalDate.now(),
+        tasksId = emptyList()
+    ),
+    Goal(
+        title = "Traveling",
+        shortPhrase = "Visit new city each month",
+        category = Category.PERSONAL,
+        startDate = LocalDate.now(),
+        endDate = LocalDate.now(),
+        tasksId = emptyList()
+    ),
+    Goal(
+        title = "Udemy course",
+        shortPhrase = "Finish udemy course",
+        category = Category.WORK,
+        startDate = LocalDate.now(),
+        endDate = LocalDate.now(),
+        tasksId = emptyList()
+    ),
+)
 
 @Composable
 actual fun GoalScreenContent() {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxSize().padding(top = 25.dp, start = 15.dp, end = 15.dp),
+        horizontalAlignment = Alignment.Start,
     ) {
-        Text("Your goals")
+        GoalHeader()
+        GoalCard()
+        AddNewGoalButton{
+
+        }
     }
+}
+
+@Composable
+fun GoalCard() {
+    LazyColumn(modifier = Modifier.height(height = 650.dp)) {
+        items(items = list){
+            GoalContent(it)
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+    }
+}
+
+
+@Composable
+fun AddNewGoalButton(onClick: () -> Unit) {
+    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+        Card(
+            modifier = Modifier.background(Color.Transparent).wrapContentSize(),
+            onClick = onClick,
+            border = BorderStroke(1.dp, Color.Black),
+            shape = RoundedCornerShape(30.dp),
+            colors = CardDefaults.outlinedCardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+        ) {
+            Row(
+                modifier = Modifier.wrapContentSize().padding(horizontal = 20.dp, vertical = 15.dp),
+                horizontalArrangement = Arrangement.spacedBy(1.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.align),
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp)
+                )
+
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Text(
+                    text = "Add new Goal",
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GoalContent(goal: Goal) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier.background(Color.Transparent)
+                .width(380.dp)
+                .height(120.dp),
+            border = BorderStroke(1.dp, Color.Black),
+            shape = RoundedCornerShape(35.dp),
+            colors = CardDefaults.outlinedCardColors(containerColor = goal.category.color),
+            onClick = {}
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 25.dp, vertical = 20.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                GoalCardHeader(goal.title, goal.category)
+
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    GoalCardFooterElement("Duration: 3 month")
+                    GoalCardFooterElement("2 tasks this week")
+                    GoalCardFooterElement("78%")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GoalCardHeader(title: String, category: Category) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.labelLarge,
+            fontSize = 22.sp
+        )
+
+        Row {
+            Text(
+                text = category.title,
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.flag),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+fun GoalCardFooterElement(text: String) {
+    Card(
+        modifier = Modifier.background(Color.Transparent).wrapContentSize(),
+        border = BorderStroke(1.dp, Color.Black),
+        shape = RoundedCornerShape(50.dp),
+        colors = CardDefaults.outlinedCardColors(containerColor = Color.Transparent),
+        onClick = {}
+    ) {
+        Row(
+            modifier = Modifier.wrapContentWidth().padding(horizontal = 8.dp, vertical = 4.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+
+@Composable
+fun GoalHeader() {
+    Row(
+        modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Your long term archiving",
+            style = MaterialTheme.typography.displayLarge,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun GoalPreview(modifier: Modifier = Modifier) {
+    GoalScreenContent()
 }
--- composeApp/src/commonMain/kotlin/com/project/lifeos/data/Goal.kt
+++ composeApp/src/commonMain/kotlin/com/project/lifeos/data/Goal.kt
@@ -1,17 +1,26 @@
 package com.project.lifeos.data
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
 import java.time.LocalDate
 
 data class Goal(
-    val id: String,
+    val id: Long? = null,
     val title: String,
-    val description: String? = null,
-    val icon: ActivityIcon, // Can be an enum representing different goal types
+    val shortPhrase: String,
+    val category: Category,
     val startDate: LocalDate,
     val endDate: LocalDate,
-    val status: GoalStatus = GoalStatus.IN_PROGRESS, // Goal status: IN_PROGRESS, COMPLETED, ABANDONED
-    val tasks: List<Task> = emptyList() // List of associated tasks
+    val status: GoalStatus = GoalStatus.IN_PROGRESS,
+    val tasksId: List<Long>
 )
+
+enum class Category(val color: Color, val title: String) {
+    SPORT(Color(0xFFA1D4DE), "Sport"),
+    STUDY(Color(0xFFE09E6F),"Study"),
+    WORK(Color(0xFFBCBCBC),"Work"),
+    PERSONAL(Color(0xFFE8DEF7), "Personal")
+}
 
 enum class GoalStatus {
     IN_PROGRESS,
