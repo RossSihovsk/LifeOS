fefdae85ff4be9a615020c65d4434abcd4b2eb1e
vetos <vetosew@gmail.com>
2024-11-20 14:21:50 +0200

    я

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.project.lifeos.App
-import com.project.lifeos.di.DesktopAppModule
 import androidx.compose.ui.window.WindowPosition
 
 
@@ -13,8 +12,7 @@ fun main() = application {
     val state = rememberWindowState(position = WindowPosition(Alignment.Center), size = DpSize(1120.dp, 900.dp))
     Window(onCloseRequest = ::exitApplication, title = "LifeOS", state = state)
     {
-        val appModule = DesktopAppModule()
-        App(appModule)
+        App()
     }
 }
 
